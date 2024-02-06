f5d22509cb6a007eb2d1d11c762e2cefa5cef13c
Clément Richard <c.richard@sstrn.com>
2024-02-06 14:05:42 +0100

    Update AppClass

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I'm a java app!");
+        System.out.println("My developper is Cléemnt RICHARD!");
     }
 }
