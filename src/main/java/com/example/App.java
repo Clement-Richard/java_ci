3974221f09ad3f0ab401390ed0663b413d80a1e2
Clément Richard <c.richard@sstrn.com>
2024-02-06 11:49:23 +0100

    Remove "Hello Earth"

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World, Hello Earth!");
+        System.out.println("Hello, World!");
     }
 }
